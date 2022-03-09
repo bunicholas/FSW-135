701311374cb780693e7bdceb422a4cff4a36b3c7
bunicholas <nicholascarroll123@gmail.com>
2022-03-08 19:55:13 -0500

    week1 updates

---
+++ week1/models/inventory.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema
+
+const invSchema = new schema({
+    itemName: {
+        type: String,
+        required: true
+    },
+    itemType: {
+        type: String,
+        required: true
+    },
+    itemPrice: {
+        type: Number,
+        required: true,
+        min: 5
+    }
+})
+
+module.exports = mongoose.model('inventory', invSchema) 
